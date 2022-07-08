0d40c08a424edf8f33fa8287a2d38068c924e2b9
Zach Cervi <zachcervi@gmail.com>
2022-07-07 20:36:32 -0600

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Zach Cervi on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
